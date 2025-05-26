5af34909c35b30ff1b7c52c0b157884511acee1f
ckatPrakash <ckatprakash@outlook.com>
2025-05-25 20:59:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ec2" {
-  source         = "./EC2_Instance"
-}
+#module "ec2" {
+#  source         = "./EC2_Instance"
+#}
 
-#module "postgres" {
-#  source = "./Postgres_Database"
+module "postgres" {
+  source = "./Postgres_Database"
 
   # You can pass EC2 outputs to this module if needed
   # For example:
   # ec2_instance_id = module.ec2.ec2_instance_id
-#}
+}
